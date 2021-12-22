dbd019a4a0d63698d0291459774f0925443c77aa
dodge <20012011ryu@gmail.com>
2021-12-22 14:03:50 +0900

    Voice Detectoion Project
    Voice Detectoion Project for Test
    Using Naver CLOVA API
    or KAKAO AI Speech AI

---
+++ VoiceDetection/VoiceDetection/Model/DataItem.cs
@@ -0,0 +1,16 @@
+﻿namespace VoiceDetection.Model
+{
+    public class DataItem
+    {
+        public string Title
+        {
+            get;
+            private set;
+        }
+
+        public DataItem(string title)
+        {
+            Title = title;
+        }
+    }
+}
